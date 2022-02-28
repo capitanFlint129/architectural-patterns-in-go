bd12082837c916f473231f3e460902a58546d859
capitanFlint129 <tekkengod@list.ru>
2022-02-28 14:44:55 +0300

    update facade usage

--- cmd/facade/main.go
+++ cmd/facade/main.go
@@ -5,14 +5,18 @@ import (
 	"go-facade/cmd/facade/pkg/facade"
 )
 
+type converter interface {
+	Convert() error
+}
+
+func useConverter(converter converter) {
+	er := converter.Convert()
+	if er != nil {
+		fmt.Println("Error in facade")
+	}
+}
+
 func main() {
 	videoConverterFacade := facade.NewVideoConverter(15, 5, 5)
-	er := videoConverterFacade.Action1()
-	if er != nil {
-		fmt.Println("Error at action 1")
-	}
-	er = videoConverterFacade.Action2(1)
-	if er != nil {
-		fmt.Println("Error at action 2")
-	}
+	useConverter(videoConverterFacade)
 }
