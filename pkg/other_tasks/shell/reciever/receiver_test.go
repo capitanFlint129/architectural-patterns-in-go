42724c26d7bafc6399154096f31c982fe71eaff0
capitanFlint129 <tekkengod@list.ru>
2022-03-30 10:49:43 +0300

    shell: add receiver tests

---
+++ pkg/other_tasks/shell/reciever/receiver_test.go
@@ -0,0 +1,63 @@
+package reciever
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"github.com/stretchr/testify/assert"
+	"sync"
+	"testing"
+)
+
+type inputData struct {
+	inputChannelData string
+}
+
+type expectedResult struct {
+	outputChannelDataLines []string
+}
+
+const (
+	receiveDataTestCaseName = "Receive data"
+)
+
+func Test_Receiver(t *testing.T) {
+	for _, testData := range []struct {
+		testCaseName   string
+		inputData      inputData
+		expectedResult expectedResult
+	}{
+		{
+			testCaseName: receiveDataTestCaseName,
+			inputData: inputData{
+				inputChannelData: "line1\nline2\nline3\n",
+			},
+			expectedResult: expectedResult{
+				outputChannelDataLines: []string{"line1", "line2", "line3"},
+			},
+		},
+	} {
+		t.Run(testData.testCaseName, func(t *testing.T) {
+			var stdin bytes.Buffer
+			stdin.Write([]byte(testData.inputData.inputChannelData))
+			scanner := bufio.NewScanner(&stdin)
+			outputChannel := make(chan string)
+			errorChannel := make(chan error)
+			receiver := NewReceiver(scanner, outputChannel, errorChannel)
+			mainCtx := context.Background()
+			ctx, cancel := context.WithCancel(mainCtx)
+			var wg sync.WaitGroup
+
+			wg.Add(1)
+			receiver.StartReceive(ctx, &wg)
+			outputChannelLines := make([]string, len(testData.expectedResult.outputChannelDataLines))
+			for i := 0; i < len(testData.expectedResult.outputChannelDataLines); i++ {
+				outputChannelLines[i] = <-outputChannel
+			}
+			cancel()
+
+			wg.Wait() // TODO вопрос: wg в тесте?
+			assert.Equal(t, testData.expectedResult.outputChannelDataLines, outputChannelLines)
+		})
+	}
+}
