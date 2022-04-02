cb1f617254857eb32846582a225317d9207b7676
capitanFlint129 <tekkengod@list.ru>
2022-04-02 09:37:48 +0300

    add ErrorCommandNotFound error

--- pkg/other_tasks/shell/errors/types.go
+++ pkg/other_tasks/shell/errors/types.go
@@ -2,7 +2,10 @@ package errors
 
 import "errors"
 
+// TODO вопрос: разделять ли ошибки по пакетам? Или оставить общий файл на весь shell?
+
 var (
 	ErrorTooManyArguments   = errors.New("too many arguments")
 	ErrorNotEnoughArguments = errors.New("not enough arguments")
+	ErrorCommandNotFound    = errors.New("command not found")
 )
