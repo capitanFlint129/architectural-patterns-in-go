c80eeb0840c98f0f3b549662a414f02b761eeb1c
capitanFlint129 <tekkengod@list.ru>
2022-04-05 17:39:03 +0300

    remove todo

--- pkg/other_tasks/shell/errors/types.go
+++ pkg/other_tasks/shell/errors/types.go
@@ -2,8 +2,6 @@ package errors
 
 import "errors"
 
-// TODO вопрос: разделять ли ошибки по пакетам? Или оставить общий файл на весь shell?
-
 var (
 	ErrorTooManyArguments   = errors.New("too many arguments")
 	ErrorNotEnoughArguments = errors.New("not enough arguments")
