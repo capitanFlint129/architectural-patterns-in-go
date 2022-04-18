6a343fac8186838ef3b71b91c45e953ceb6df5a7
capitanFlint129 <tekkengod@list.ru>
2022-04-18 10:36:39 +0300

    rework types

--- pkg/other_tasks/calendar/types/types.go
+++ pkg/other_tasks/calendar/types/types.go
@@ -12,9 +12,16 @@ type EventHandlerData struct {
 	Event  Event `json:"event"`
 }
 
-type DateHandlerData struct {
-	UserId int       `json:"user_id"`
-	Date   time.Time `json:"date"`
+type UpdateEventHandlerData struct {
+	UserId   int   `json:"user_id"`
+	Event    Event `json:"event"`
+	NewEvent Event `json:"new_event"`
+}
+
+type DateIntervalHandlerData struct {
+	UserId    int       `json:"user_id"`
+	StartDate time.Time `json:"start_date"`
+	EndDate   time.Time `json:"end_date"`
 }
 
 type EventResponse struct {
